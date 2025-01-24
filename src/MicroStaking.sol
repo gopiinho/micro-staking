ed4005dc9d2e308414cf5f75d433916b648295bd
Gurpreet <gs109690@gmail.com>
2025-01-24 13:54:53 +0530

    token contract mint and burn

--- src/MicroStaking.sol
+++ src/MicroStaking.sol
@@ -6,6 +6,14 @@ import {Owned} from "solmate/auth/Owned.sol";
 
 contract Token is ERC20, Owned {
     constructor() ERC20("Token", "TKN", 18) Owned(msg.sender) {}
+
+    function mint(address to, uint256 amount) external onlyOwner {
+        _mint(to, amount);
+    }
+
+    function burn(address from, uint256 amount) external onlyOwner {
+        _burn(from, amount);
+    }
 }
 
 contract MicroStaking {}
