41bfcebdd499889397b9cffd2f452af114e93acb
Gurpreet <gs109690@gmail.com>
2025-01-30 22:54:00 +0530

    Split contracts

---
+++ src/Token.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.25;
+
+import {ERC20} from "solmate/tokens/ERC20.sol";
+import {Owned} from "solmate/auth/Owned.sol";
+
+contract Token is ERC20, Owned {
+    constructor() ERC20("Token", "TKN", 18) Owned(msg.sender) {}
+
+    function mint(address to, uint256 amount) external onlyOwner {
+        _mint(to, amount);
+    }
+
+    function burn(address from, uint256 amount) external onlyOwner {
+        _burn(from, amount);
+    }
+}
